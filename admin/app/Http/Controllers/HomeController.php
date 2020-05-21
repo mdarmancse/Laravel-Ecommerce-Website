7409f88e6ebe9eab92c4a48eaf031d17c8c60a68
Md Arman Ullah <65160577+mdarmancse@users.noreply.github.com>
2020-05-21 17:30:16 +0600

    Visitor Page Ready

--- admin/app/Http/Controllers/HomeController.php
+++ admin/app/Http/Controllers/HomeController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class HomeController extends Controller
 {
-    //
+    function HomeIndex(){
+
+        return view('Home');
+    }
+
 }
